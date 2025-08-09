fe61057d96742909c7c19bfb6bc527eccab4d1b6
PATRICIA DE SOUSA RIBEIRO <patriciaribeiro554@gmail.com>
2025-08-09 20:44:47 -0300

    Initial commit with frontend folder

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+   return (
+    <div className="min-h-screen flex items-center justify-center bg-blue-500 text-white text-3xl font-bold">
+      Tailwind está funcionando 🚀
+    </div>
+  );
+}
+
+export default App;
